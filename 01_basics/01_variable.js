5e3e1d0053e52d7fbca5e4794ca86ca27487f120
py516039 <142260249+py516039@users.noreply.github.com>
2024-10-08 14:57:33 +0000

    concept of variables

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accountEmail = "hitesh@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 2
+
+accountEmail = "hc@.com"
+accountPassword = "54235"
+accountCity = "Gorakhpur"
+
+console.log(accountId)
+
+// never use var 
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
