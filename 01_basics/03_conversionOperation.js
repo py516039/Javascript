362f0d7c240d8fcbde03394f7cb579926008e568
py516039 <142260249+py516039@users.noreply.github.com>
2024-10-09 15:14:16 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,21 @@
+// let score = 33;
+
+// console.log(typeof(score));
+
+//"33" => 33
+// "33abc" => NaN 
+// true => 1; false => 0;
+
+// let isLoggedIn = 1;
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);//
+
+// 1 => true; 0 => false;
+// "" => false
+// "Prince" => true
+
+let SomeNumber = 33
+
+let stringNumber = String(SomeNumber);
+console.log(stringNumber);
+console.log(typeof(stringch));
