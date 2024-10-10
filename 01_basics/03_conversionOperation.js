3477ba84971422888eb614383080b4c7e2e9dc24
py516039 <142260249+py516039@users.noreply.github.com>
2024-10-10 15:32:35 +0000

    Conversion Operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -14,8 +14,41 @@
 // "" => false
 // "Prince" => true
 
-let SomeNumber = 33
+// let SomeNumber = 33
 
-let stringNumber = String(SomeNumber);
-console.log(stringNumber);
-console.log(typeof(stringch));
+// let stringNumber = String(SomeNumber);
+// console.log(stringNumber);
+// console.log(typeof(stringch));
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+// Some Operations
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3)//power form
+console.log(2/2);
+console.log(10%5);
+
+let str1 = "Hello ";
+let str2 = "Prince";
+
+let str3 = str1+str2;
+console.log(str3)
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1+2+"2");
+
+console.log ((3+4) * 5 % 3);
+console.log(+true)
+
+let num1, num2, num3;
+ num1 = num2 = num3 = 2+2;
+
+ let gameCounter = 100;
+ gameCounter++
+ console.log();
