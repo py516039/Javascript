0e94a25c3c17dab5c125e310a029a60f88e5f914
py516039 <142260249+py516039@users.noreply.github.com>
2024-10-13 12:40:16 +0000

    math and number

---
+++ 01_basics/06_numbers&Math.js
@@ -0,0 +1,30 @@
+// const score = 400;
+// console.log(score);
+
+// const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+// const otherNumber = 23.8966;
+// console.log(otherNumber.toPrecision());
+
+// const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));s
+
+// +++++++++++++++++++++Maths++++++++++++++++++++++++++++++++++++
+console.log(Math);
+console.log(Math.abs(-4)) // absolute value
+console.log(Math.round(4.6));// round off
+console.log(Math.ceil(4.2));//upper value i.e.=5;
+console.log(Math.floor(4.9));//lower value i.e.=4;
+console.log(Math.min(4,3,6,8));
+console.log(Math.max(4,3,6,8));
+
+console.log(Math.random());// always gives values b/w 0 and 1;
+
+
+ 
+
+
